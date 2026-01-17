3f08d442dce2acda448a16aa69fb7c34f8eabe49
Faisal Khan <faisal5189126@gmail.com>
2026-01-18 02:26:26 +0530

    updated cloud mysql

--- src/main/java/com/project/ecom/WebStoreApplication.java
+++ src/main/java/com/project/ecom/WebStoreApplication.java
@@ -2,8 +2,12 @@ package com.project.ecom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.project.ecom")
+@EnableJpaRepositories(basePackages = "com.project.ecom")
 public class WebStoreApplication {
     public static void main(String[] args) {
 
